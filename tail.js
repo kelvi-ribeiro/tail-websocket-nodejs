224c8185a035ed43ceb2148ddca2b814b8db3a78
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2019-08-14 16:17:18 -0300

    Usando tail para ler o e monitorar o arquivo
    Devido a refatoração, alguns imports, techos de código foram removidos

--- tail.js
+++ tail.js
@@ -1,80 +1,41 @@
-#!/usr/bin/env node
-var fs = require('fs'),
-    port = process.env.npm_package_config_port || 8090,
-    express = require('express'),
-    http = require('http'),
-    app = express(),
-    server = http.createServer(app),
-    io = require('socket.io').listen(server),
-    files = [],
-    os = require('os'),
-    buf = new Buffer(4096);
+const express = require('express');
+const app = express();
+const port = process.env.npm_package_config_port || 8090;
+const http = require('http')
+const server = http.createServer(app);
+const io = require('socket.io').listen(server);
+const Tail = require('tail').Tail;
+const tail = new Tail(__dirname + '/public/file.txt');
+const files = []
 
-server.listen(port);
-
-if (process.argv.length < 3) {
-    console.error("Usage: " + __filename + " filename");
-    process.exit(1);
-}
-
-// server the browser dependencies
 app.use(express.static('public'));
 app.use(express.static('files'));
+
 app.use('/static', express.static('public'));
-// server socket.io client to the browser
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
 app.get('/socket.io/socket.io.js', function (req, res) {
     res.sendfile(__dirname + '/node_modules/socket.io-client/dist/socket.io.js');
 });
 // open a route for each file from the command line
-app.get('/files/:filename', function (req, res) {
+app.get('/files/0', function (req, res) {
     res.sendfile(__dirname + '/public/index.html');
 });
 
-// for each file from the command line open an route
-process.argv.splice(2).forEach(function (filename) {
-    "use strict";
-    fs.open(filename, 'r', function (err, fd) {
-        if (err) {
-            console.error('Unable to open: ' + filename);
-            return;
-        }
+tail.on("line", function (data) {
+    var nsName = '/files/0',
+        ns = io.of(nsName)
+            .on('connection', function (socket) {
+                socket.emit('files', files);
+            });
 
-        files.push(filename);
-        var nsName = '/files/' + (-1 + files.length),
-            ns = io.of(nsName)
-                .on('connection', function(socket) {
-                    socket.emit('files', files);
-                });
-
-        console.info('Listening on http://' + os.hostname() + ':' + port + nsName + ' ' + filename);
-
-        // watch file
-        fs.watchFile(filename, function (curr, prev) {
-            var len = curr.size - prev.size, position = prev.size;
-            if (len > 0) {
-                fs.read(fd, buf, 0, len, position,
-                    function (err, bytesRead, buffer) {
-                        if (err) {
-                            console.error(err);
-                            return;
-                        }
-                        var msg = buffer.toString('utf8', 0, bytesRead);
-                        ns.emit('message', msg);
-                    });
-            } else {
-                console.log(curr);
-            }
-        });
-    });
+    ns.emit('message', data);
+    console.log(data);
 });
 
-console.info('Listing on http://' + os.hostname() + ':' + port);
 
-// on connection broadcast file names the server is tailing
-io.sockets
-    .on('connection', function (socket) {
-        "use strict";
 
-        // broadcast opened files
-        socket.emit('files', files);
-    });
+
+server.listen(port);
