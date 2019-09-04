47c457f437b8ffe64680bb94f9d9832094b15808
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2019-09-04 13:18:18 -0300

    Criando array de tails a partir de files e iterando para criar um socket para cada file

--- tail.js
+++ tail.js
@@ -5,10 +5,11 @@ const http = require('http')
 const server = http.createServer(app);
 const io = require('socket.io').listen(server);
 const Tail = require('tail').Tail;
-//const options = {separator: /[\r]{0,1}\n/, fsWatchOptions: {}, follow: true, logger: console,flushAtEOF:true}
-const tail = new Tail('C:\\Users\\kelvi.ribeiro\\git\\processoSeletivo\\logs\\production_profile_app.log');
-const files = []
-
+const files = [
+                  {codigo:'controle-acesso', path:'C:\\Users\\kelvi.ribeiro\\Documents\\documentacao-scripts-sql-anotacoes\\duvidas-do-projeto.txt'}
+                , {codigo:'test', path:'C:\\Users\\kelvi.ribeiro\\Documents\\documentacao-scripts-sql-anotacoes\\anotacoes.txt'}
+              ]
+const tails = files.map(file => new Tail(file.path));
 app.use(express.static('public'));
 app.use(express.static('files'));
 
@@ -21,22 +22,23 @@ app.get('/socket.io/socket.io.js', function (req, res) {
     res.sendFile(__dirname + '/node_modules/socket.io-client/dist/socket.io.js');
 });
 // open a route for each file from the command line
-app.get('/files/0', function (req, res) {
+app.get('/files/:codigoFile', function (req, res) {    
     res.sendFile(__dirname + '/public/index.html');
 });
 
-tail.on("line", function (data) {
-    var nsName = '/files/0',
-        ns = io.of(nsName)
-            .on('connection', function (socket) {                
-                socket.setMaxListeners(15)
-                socket.emit('files', files);
-            });
-
-    ns.emit('message', data);    
+tails.forEach(tail => {
+    tail.on("line", function (data) {        
+        files.forEach(file => {
+            var nsName = file.codigo,
+            ns = io.of(nsName)
+                .on('connection', function (socket) {                
+                    socket.setMaxListeners(15)
+                    socket.emit('files', files);
+                });
+    
+            ns.emit('message', data);            
+        });
+    });
 });
-
-
-
 
 server.listen(port);
