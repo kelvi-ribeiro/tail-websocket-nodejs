46383c718a333c7bbcf8f8d42d4cc7c3633d878b
kelvi-ribeiro <kelvi.ribeiro@gmail.com>
2019-09-04 22:18:14 -0300

    Tratando 404 no files/:codigoFile e adicionando name a cada objeto de file

--- tail.js
+++ tail.js
@@ -7,8 +7,8 @@ const io = require('socket.io').listen(server);
 const Tail = require('tail').Tail;
 var ip = require('ip');
 const files = [
-                  {codigo:'/files/controle-acesso', path:'./testFiles/file_1.txt'}
-                , {codigo:'/files/test', path:'./testFiles/file_2.txt'}
+                  {codigo:'controle-acesso', name:'Controle de Acesso', path:'./testFiles/file_1.txt'}
+                , {codigo:'financeiro', name:'Financeiro', path:'./testFiles/file_2.txt'}
               ]
 const tails = files.map(file => new Tail(file.path));
 app.use(express.static('public'));
@@ -23,18 +23,23 @@ app.get('/socket.io/socket.io.js', function (req, res) {
     res.sendFile(__dirname + '/node_modules/socket.io-client/dist/socket.io.js');
 });
 // open a route for each file from the command line
-app.get('/files/:codigoFile', function (req, res) {    
-    res.sendFile(__dirname + '/public/index.html');
+app.get('/files/:codigoFile', function (req, res) {       
+    if(files.find(file => file.codigo === req.params.codigoFile)){
+        return res.sendFile(__dirname + '/public/index.html');
+    } else {
+        return res.status(404).send('Arquivo não encontrado')
+    } 
+
 });
 
 files.forEach(file => {
-    var nsName = file.codigo,
+    var nsName = '/files/' + file.codigo,
     ns = io.of(nsName)
     .on('connection', function (socket) {                
         socket.setMaxListeners(15)
         socket.emit('files', files);
     });        
-    console.log(`Listening on http://${ip.address()}:${port}${file.codigo}`);
+    console.log(`Listening on http://${ip.address()}:${port}/${file.codigo}`);
 
     tails.forEach(tail => {      
         tail.on("line", function (data) {                       
